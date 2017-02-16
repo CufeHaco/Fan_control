66cd9381713632d99ec6c1be1bd6bbb1e13efbcb
CufeHaco <crazyrabbitking89@gmail.com>
2017-02-16 18:44:29 -0500

    Update Fan_init.rb

--- Fan_init.rb
+++ Fan_init.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
 
-system 'sudo ruby ./fan_controller.rb'
+#---------------------------------------------------
+#This script starts the fan driver as a super user.
+#change the path dir to match yours, by default it
+#should be /home/pi/Fan_control/Fan_controller.rb"
+#to have this driver working when the RPI boots,
+#sudo nano /etc/rc.local
+#add this before the exit(0) line in rc.local
+#ruby /home/pi/Fan_control/Fan_init.rb &
+#save and exit, then reboot.
+#---------------------------------------------------
+system "sudo ruby /home/pi/Fan_control/Fan_controller.rb"
+sleep(1)
+exit(0)
